bccf3bde11b6727b8dd9f369a90ae577df820dbc
rubenmartins21 <rubenmartins463@gmail.com>
2023-05-20 13:46:39 -0100

    add navigation to user registration page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,20 +1,27 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
-import { Button } from "@mui/material";
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
+  const navigate = useNavigate();
+
+  const handleCadastrarClick = () => {
+    navigate("/user-registration");
+  };
+
   return (
     <div className="border-b border-gray-200 p-2 flex items-center justify-start w-full">
-      <Typography variant="h5" className="">
-        Come In
-      </Typography>
+      <Typography variant="h5">Come In</Typography>
       <Button
         sx={{ marginLeft: "auto", marginRight: "0.5rem" }}
         variant="contained"
       >
         Entrar
       </Button>
-      <Button variant="outlined">Cadastrar</Button>
+      <Button onClick={handleCadastrarClick} variant="outlined">
+        Cadastrar
+      </Button>
     </div>
   );
 };
